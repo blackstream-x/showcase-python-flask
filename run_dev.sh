1b14272d602863b740f06179a1c17075902564ae
blackstream-x <rainer@blackstream.de>
2021-10-21 17:10:50 +0200

    Add hint to run the project in a virtual environment

--- run_dev.sh
+++ run_dev.sh
@@ -1,2 +1,7 @@
+if [ -z "${VIRTUAL_ENV}" ] ; then
+    echo "Please run this project in a virtual environment!"
+    exit 1
+fi
+
 . ./set_environment_dev.sh
-python3 -m flask run
+python -m flask run
